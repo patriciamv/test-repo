073fb4519599e0fed93dff1b90b5e3ce5bdad36d
pmarquez <pmarquez@digitalorigin.com>
2018-04-27 09:56:59 +0200

    added calculator file

---
+++ calculator.R
@@ -0,0 +1,17 @@
+# Program make a simple calculator that can add, subtract, multiply and divide using functions
+add <- function(a, b) {
+   
+}
+
+subtract <- function(a, b) {
+   
+}
+
+multiply <- function(a, b) {
+   
+}
+
+divide <- function(a, b) {
+   
+}
+
